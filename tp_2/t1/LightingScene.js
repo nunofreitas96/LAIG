d389fdf7cd4b33e4e7f32fcb35b9a9e229ee8048
filipaivars <ivars.filipa@gmail.com>
2016-10-03 13:16:45 +0100

    materiais e luzes

--- tp_2/t1/LightingScene.js
+++ tp_2/t1/LightingScene.js
@@ -44,14 +44,13 @@ LightingScene.prototype.init = function(application) {
 	this.materialDefault = new CGFappearance(this);
 
 	this.materialA = new CGFappearance(this);
-	this.materialA.setAmbient(0.3,0.3,0.3,1);
-	this.materialA.setDiffuse(0.6,0.6,0.6,1);
-	//this.materialA.setSpecular(0.2,0.2,0.2,1);	// 2.6 -> comentar
-	//this.materialA.setSpecular(0.8,0.8,0.8,1);		// 2.6 -> mat(A)=mat(B)
-	//this.materialA.setSpecular(0,0,0.8,1);			// 2.8
+	this.materialA.setAmbient(0.11,0.42,0.5,1);
+	//this.materialA.setDiffuse(0.6,0.6,0.6,1);
+  this.materialA.setDiffuse(0.11,0.42,0.5,1);
 	this.materialA.setSpecular(0,0.2,0.8,1);		// 2.9
-	//this.materialA.setShininess(10);
-	this.materialA.setShininess(120);				// 2.7
+	this.materialA.setShininess(10);
+	//this.materialA.setShininess(120);				// 2.7
+  this.materialA.setEmission(1,1,1,1);
 
 	this.materialB = new CGFappearance(this);
 	this.materialB.setAmbient(0.3,0.3,0.3,1);
@@ -152,9 +151,11 @@ LightingScene.prototype.initLights = function() {
 
 	// Positions for four lights
 	this.lights[0].setPosition(4, 6, 1, 1);
-	this.lights[1].setPosition(10.5, 6.0, 1.0, 1.0);
+	this.lights[1].setPosition(5, 5, 8, 1.0);
 	this.lights[2].setPosition(10.5, 6.0, 5.0, 1.0);	// 3.1
 	this.lights[3].setPosition(4, 6.0, 5.0, 1.0);
+
+
 
 	this.lights[0].setAmbient(0, 0, 0, 1);
 	this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
@@ -172,14 +173,14 @@ LightingScene.prototype.initLights = function() {
 	this.lights[2].setConstantAttenuation(0);	// 3.2 -> kc=0
 	this.lights[2].setLinearAttenuation(1);	// 3.2 -> kl=0.2
 	this.lights[2].setQuadraticAttenuation(0);	// 3.2 -> kq=0
-	//this.lights[2].enable();
+	this.lights[2].enable();
 
 	this.lights[3].setAmbient(0, 0, 0, 1);
 	this.lights[3].setDiffuse(1.0, 1.0, 1.0, 1.0);
 	this.lights[3].setSpecular(1,1,0, 1.0);
-	this.lights[2].setConstantAttenuation(0);
-	this.lights[2].setLinearAttenuation(0);
-	this.lights[2].setQuadraticAttenuation(1);
+	this.lights[3].setConstantAttenuation(0);
+	this.lights[3].setLinearAttenuation(0);
+	this.lights[3].setQuadraticAttenuation(1);
 	this.lights[3].enable();
 
 	this.shader.unbind();

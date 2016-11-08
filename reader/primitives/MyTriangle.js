f3ab0ebe3ebaa1d75bc78d568559d2257b2c78a4
filipaivars <ivars.filipa@gmail.com>
2016-11-08 17:27:32 +0000

    Revert "Triangle textures correctly mapped"
    This reverts commit 07d0336bed5db4f915ce9b5fe1ac9118b65c77f1.

--- reader/primitives/MyTriangle.js
+++ reader/primitives/MyTriangle.js
@@ -60,10 +60,10 @@ MyTriangle.prototype.initBuffers = function() {
 }
 
 
-MyTriangle.prototype.textResize = function(length_s, length_t) {
+MyTriangle.prototype.scaleTexCoords = function(ampS, ampT) {
 	for (var i = 0; i < this.texCoords.length; i += 2) {
-		this.texCoords[i] = this.originalTexCoords[i] / length_s;
-		this.texCoords[i + 1] = this.originalTexCoords[i+1] / length_t;
+		this.texCoords[i] = this.originalTexCoords[i] / ampS;
+		this.texCoords[i + 1] = this.originalTexCoords[i+1] / ampT;
 	}
 
 	this.updateTexCoordsGLBuffers();

4b760e14c65d64a0ea8e2199f70a3c7490b5b0ba
filipaivars <ivars.filipa@gmail.com>
2016-11-09 02:11:44 +0000

    prob1 done

--- teste_filipa/prob1/reader/primitives/MyDiamond.js
+++ teste_filipa/prob1/reader/primitives/MyDiamond.js
@@ -9,57 +9,43 @@ function MyDiamond(scene, slices) {
 	CGFobject.call(this,scene);
 	this.slices = slices;
 
-  this.height = Math.sqrt(1+(.5^2));
-  this.distance = Math.cos(Math.PI/this.slices);
-  this.deg=180/Math.PI;
-  var x = .5*Math.sin(Math.PI/this.slices);
-	this.triangle=new MyTriangle(scene, -x,0,0, x,0,0, 0,this.height,0);
+	this.angleA = Math.atan(1/.5);
+	this.angleB = Math.PI/this.slices;
+	this.halfX = .5*Math.sin(this.angleB);
+	this.hY = .5*Math.cos(this.angleB);
+	this.aHip = Math.sqrt(1+Math.pow(.5,2));
+	this.h = Math.sqrt(Math.pow(this.aHip,2)-Math.pow(this.halfX,2));
+
+	this.triangle=new MyTriangle(scene, -this.halfX,0,0, this.halfX,0,0, 0,this.h,0);
 	this.triangle.initBuffers();
 };
 
 MyDiamond.prototype = Object.create(CGFobject.prototype);
 MyDiamond.prototype.constructor=MyDiamond;
 
-
-
 MyDiamond.prototype.display = function () {
-  for (var i = 0; i < 2; i++) {
+	var distance = .5;
+
+  for (var i = 0; i < this.slices; i++) {
     this.scene.pushMatrix();
 
-      //this.scene.rotate(i*2*Math.PI/this.slices, 0, 1, 0);
-      this.scene.translate(0,0,this.distance);
-      this.scene.rotate(2*Math.PI-Math.atan(1/.5),1,0,0);
+      this.scene.rotate(i*2*Math.PI/this.slices, 0, 1, 0);
+      this.scene.translate(0,0,this.hY);
+			this.scene.rotate(-(Math.PI/2 - this.angleA -.055),1,0,0);
 
       this.triangle.display();
     this.scene.popMatrix();
   }
+this.scene.rotate(Math.PI, 1, 0, 0);
+	for (var i = 0; i < this.slices; i++) {
+		this.scene.pushMatrix();
 
-/*
-	this.scene.translate(0,0,0.5);
-	this.quad.display();
-	this.scene.translate(0,0,-0.5);
-	this.scene.rotate(-0.5*Math.PI,1,0,0);
-	this.scene.translate(0,0,0.5);
-	this.scene.rotate(-0.5*Math.PI,0,0,1);
-	this.quad.display();
-	this.scene.translate(0,0,-1);
-	this.scene.rotate(Math.PI,0,1,0);
-	this.scene.rotate(0.5*Math.PI,0,0,1);
-	this.quad.display();
-	this.scene.translate(0,0,-0.5);
-	this.scene.rotate(0.5*Math.PI,1,0,0);
-	this.scene.translate(0,0,0.5);
-	this.scene.rotate(1.5*Math.PI,0,0,1);
-	this.quad.display();
-	this.scene.translate(0,0,-0.5);
-	this.scene.rotate(1.5*Math.PI,1,0,0);
-	this.scene.translate(0,0,0.5);
-	this.quad.display();
-	this.scene.translate(0,0,-1);
-	this.scene.rotate(0.5*Math.PI,0,1,0);
-	this.scene.rotate(2*Math.PI,0,0,1);
-	this.scene.rotate(0.5*Math.PI,0,1,0);
-	*/
-//	this.triangle.display();
+			this.scene.rotate(i*2*Math.PI/this.slices, 0, 1, 0);
+			this.scene.translate(0,0,this.hY);
+			this.scene.rotate(-(Math.PI/2 - this.angleA -.055),1,0,0);	//erro numerico: .055
+
+			this.triangle.display();
+		this.scene.popMatrix();
+	}
 
 };
